388670b2d91e7e3d7bb1bdf767b0db76b1b5ff55
GraemeBP <graemebphillips@gmail.com>
2019-11-27 16:22:32 +0000

    BS Options into Class

--- BS_Options.py
+++ BS_Options.py
@@ -1,31 +1,55 @@
 import numpy as np
 import scipy.stats as si
 
-def BSCall(S, K, T, Sigma, d, r):
 
-    """
-    Calculate European Call Price based on the Black-Scholes models:
-        The formula can be found here: https://en.wikipedia.org/wiki/Black%E2%80%93Scholes_model
+class BlackScholes():
 
-    :function si.norm.cdf: The cumulative distribution function of the standard normal distribution
-    :param S:       Stock price of the underlying security
-    :param K:       The strike price of the option, also frequently called exercise price.
-    :param T:       Time to maturity in years.
-    :param Sigma:   The volatility of returns of the underlying asset
-    :param d:       The dividend yield. An annual rate, expressed in terms of continuous compounding.
-    :param r:       The risk free rate. An annual rate, expressed in terms of continuous compounding.
-    :return:        The price of a European call option via the Black Scholes Model
-    """
-    d1 = (np.log(S / K) + (r - d + 0.5*Sigma**2)*T) / (np.sqrt(T)*Sigma)
-    d2 = (np.log(S / K) + (r - d - 0.5*Sigma**2)*T) / (np.sqrt(T)*Sigma)
+    def __init__(self, s, k, t, sigma, d, r):
+        self.s = s
+        self.k = k
+        self.t = t
+        self.sigma = sigma
+        self.d = d
+        self.r = r
 
-    call_price = (S * np.exp(-d*T) * si.norm.cdf(d1, 0.0, 1.0) - K * np.exp(-r * T) * si.norm.cdf(d2, 0.0, 1.0))
-    return call_price
-graeme  = BSCall(100,100,1,0.1,0,0.05)
+    def bscall(self):
+        """
+        Calculate European Call Price based on the Black-Scholes models:
+            The formula can be found here: https://en.wikipedia.org/wiki/Black%E2%80%93Scholes_model
 
-def BSPut(S, K, T, Sigma, d, r):
-    d1 = (np.log(S / K) + (r - d + 0.5*Sigma**2)*T) / (np.sqrt(T)*Sigma)
-    d2 = (np.log(S / K) + (r - d - 0.5*Sigma**2)*T) / (np.sqrt(T)*Sigma)
+        :function si.norm.cdf: The cumulative distribution function of the standard normal distribution
+        :param S:       Stock price of the underlying security
+        :param K:       The strike price of the option, also frequently called exercise price.
+        :param T:       Time to maturity in years.
+        :param Sigma:   The volatility of returns of the underlying asset
+        :param d:       The dividend yield. An annual rate, expressed in terms of continuous compounding.
+        :param r:       The risk free rate. An annual rate, expressed in terms of continuous compounding.
+        :return:        The price of a European call option via the Black Scholes Model
+        """
+        d1 = (np.log(self.s / self.k) + (self.r - self.d + 0.5*self.sigma**2)*self.t) / (np.sqrt(self.t)*self.sigma)
+        d2 = (np.log(self.s / self.k) + (self.r - self.d - 0.5*self.sigma**2)*self.t) / (np.sqrt(self.t)*self.sigma)
 
-    put_price = (K * np.exp(-r * T) * si.norm.cdf(-d2, 0.0, 1.0)) - (S * np.exp(-d*T) * si.norm.cdf(-d1, 0.0, 1.0))
-    return put_price
+        call_price = (self.s * np.exp(-self.d*self.t) * si.norm.cdf(d1, 0.0, 1.0)
+                      - self.k * np.exp(-self.r * self.t) * si.norm.cdf(d2, 0.0, 1.0))
+        return call_price
+
+    def bsput(self):
+        """
+        Calculate European Put Price based on the Black-Scholes models:
+            The formula can be found here: https://en.wikipedia.org/wiki/Black%E2%80%93Scholes_model
+
+        :function si.norm.cdf: The cumulative distribution function of the standard normal distribution
+        :param S:       Stock price of the underlying security
+        :param K:       The strike price of the option, also frequently called exercise price.
+        :param T:       Time to maturity in years.
+        :param Sigma:   The volatility of returns of the underlying asset
+        :param d:       The dividend yield. An annual rate, expressed in terms of continuous compounding.
+        :param r:       The risk free rate. An annual rate, expressed in terms of continuous compounding.
+        :return:        The price of a European put option via the Black Scholes Model
+        """
+        d1 = (np.log(self.s / self.k) + (self.r - self.d + 0.5*self.sigma**2)*self.t) / (np.sqrt(self.t)*self.sigma)
+        d2 = (np.log(self.s / self.k) + (self.r - self.d - 0.5*self.sigma**2)*self.t) / (np.sqrt(self.t)*self.sigma)
+
+        put_price = (self.k * np.exp(-self.r * self.t) * si.norm.cdf(-d2, 0.0, 1.0)) \
+                    - (self.s * np.exp(-self.d*self.t) * si.norm.cdf(-d1, 0.0, 1.0))
+        return put_price
