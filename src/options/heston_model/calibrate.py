2ca69364c8a71042d94ab09da26fcd614d6de33c
GraemeBP <graemebphillips@gmail.com>
2020-01-13 23:04:55 +0000

    Updating graph function to work with Mesh data

--- src/options/heston_model/calibrate.py
+++ src/options/heston_model/calibrate.py
@@ -1,6 +1,8 @@
 from scipy.optimize import minimize, differential_evolution
 from src.options.heston_model.heston_model import HestonModel
-from numpy import array
+from numpy import array, linspace, meshgrid
+from scipy.interpolate import griddata
+
 import pandas as pd
 import matplotlib.pyplot as plt
 from mpl_toolkits import mplot3d
@@ -27,7 +29,7 @@ class CalibrateHeston:
     def objective(self, guess):
         sum_of_relative_difference = 0
         # HestonModel(s, k, t, v, r, theta, kappa, sigma, rho)
-        for i in range(0, 4):
+        for i in range(0, 8):
 
             heston = HestonModel(self.known_s[i], self.known_k[i], self.known_t[i], guess[0], self.known_r[i],
                                  guess[1], guess[2], guess[3], guess[4])
@@ -62,7 +64,7 @@ class CalibrateHeston:
         strike_price = []
         maturity = []
 
-        for i in range(0, 4):
+        for i in range(0, 8):
 
             heston = HestonModel(self.known_s[i], self.known_k[i], self.known_t[i], v_calibrated, self.known_r[i],
                                  theta_calibrated, kappa_calibrated, sigma_calibrated, rho_calibrated)
@@ -71,10 +73,17 @@ class CalibrateHeston:
             strike_price.append(self.known_k[i])
             maturity.append(self.known_t[i])
 
-            print(call_price)
+        # fig.plot3D(strike_price, maturity, call_price, 'gray')
+        plotx, ploty = meshgrid(linspace(min(maturity), max(maturity), 10), linspace(min(strike_price), max(strike_price), 10))
+        plotz = griddata((maturity,strike_price),call_price,(plotx,ploty),method = 'linear')
 
-        fig = plt.axes(projection='3d')
-        fig.plot3D(strike_price, maturity, call_price, 'gray')
+        fig = plt.figure()
+        fig = fig.add_subplot(111,projection = '3d')
+        fig.plot_surface(plotx,ploty,plotz,cstride = 1,rstride = 1,cmap = 'viridis')
+
+        fig.set_ylabel('Strike Price')
+        fig.set_xlabel('Time to Maturity')
+        fig.set_zlabel('Call Price')
 
     def run_optimisation(self):
         if self.optimisation_type == 'local':
